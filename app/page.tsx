16d0c3b6eaaa776479bb5bf1f84bc7e0cc473e52
JayantOlhyan <jayantolhyan@gmail.com>
2026-09-10 06:09:16 +0530

    Mount CtaBand Instance D (Blocks 292-295)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components/Hero";
 import { TrustBar } from "@/components/TrustBar";
 import { WhoThisIsFor } from "@/components/WhoThisIsFor";
-import { CtaBandA, CtaBandB, CtaBandC } from "@/components/CtaBand";
+import { CtaBandA, CtaBandB, CtaBandC, CtaBandD } from "@/components/CtaBand";
 import { Benefits } from "@/components/Benefits";
 import { WhatToExpect } from "@/components/WhatToExpect";
 import { Curriculum } from "@/components/Curriculum";
@@ -35,6 +35,9 @@ export default function Home() {
 
       {/* Section 6: Curriculum (Blocks 60-74, 83-140, 142-164, 165-291, 82) */}
       <Curriculum />
+
+      {/* CTA Band Instance D (Blocks 292-295) */}
+      <CtaBandD />
 
       {/* Target anchor for Hero CTA scroll */}
       <div id="lead-form" className="sr-only" aria-hidden="true" />
